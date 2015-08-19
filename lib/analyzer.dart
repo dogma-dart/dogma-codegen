a6f2646b2d79f5a6596a7c5c31d1f506c049cb82
Don <don.j.olmstead@gmail.com>
2015-08-19 08:21:11 -0700

    Add analyzer.dart

---
+++ lib/analyzer.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2015, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_codegen.analyzer;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/analyzer/analyzer_metadata.dart';
+export 'src/analyzer/context.dart';
