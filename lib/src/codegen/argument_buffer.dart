bc2d9e2a5a83319e8325900d8182508d81d4b29a
Don <don.j.olmstead@gmail.com>
2015-08-30 22:09:47 -0700

    Cleaning up ArgumentBuffer definition

--- lib/src/codegen/argument_buffer.dart
+++ lib/src/codegen/argument_buffer.dart
@@ -6,29 +6,53 @@
 /// Contains the [ArgumentBuffer] class.
 library dogma_codegen.src.codegen.argument_buffer;
 
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// A wrapper over a string buffer that handles writing a list of arguments.
+///
+/// The [ArgumentBuffer] keeps track of the number of times [write] was called
+/// and writes the [separator] during successive calls.
 class ArgumentBuffer {
+  //---------------------------------------------------------------------
+  // Member variables
+  //---------------------------------------------------------------------
+
   /// The separator to use.
   final String separator;
   /// The string buffer to write to.
-  StringBuffer _buffer = new StringBuffer();
-  /// Whether the separator should be used.
-  bool _useSeparator = false;
+  final StringBuffer _buffer = new StringBuffer();
 
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of the [ArgumentBuffer] class.
+  ///
+  /// If no separator is specified then ',' is used.
   ArgumentBuffer([this.separator = ',']);
 
+  /// Creates an instance of the [ArgumentBuffer] class where arguments are
+  /// separated using a ',' and a line break.
   ArgumentBuffer.lineBreak()
       : separator = ',\n';
 
+  //---------------------------------------------------------------------
+  // Public methods
+  //---------------------------------------------------------------------
+
+  /// Writes the argument [value] to the buffer.
   void write(String value) {
-    if (_useSeparator) {
+    // Write the separator if the buffer is not empty
+    if (_buffer.isNotEmpty) {
       _buffer.write(separator);
-    } else {
-      _useSeparator = true;
     }
 
     _buffer.write(value);
   }
 
+  /// Returns the contents of the buffer.
   @override
   String toString() => _buffer.toString();
 }
