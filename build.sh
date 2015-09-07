57405b924a9281ffbe6ea070388a155483f273b5
Don <don.j.olmstead@gmail.com>
2015-09-07 09:50:14 -0700

    Cleanup build

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
-set -e
+#!/bin/sh
+set -ex
 
+# Clone dependencies
 git clone https://github.com/dogma-dart/dogma-data.git ../dogma-data
 
+# Get version
+dart --version
+
+# Install dependencies
 pub install
 
+# Run the linter
 pub global activate linter
 pub global run linter .
 
+# Run the tests
 dart --checked test/all.dart
 
+# Run the tests and report back to coveralls
 if [ "$COVERALLS_TOKEN" ]; then
   pub global activate dart_coveralls 0.2.0
   pub global run dart_coveralls report \
