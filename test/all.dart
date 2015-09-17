9022e4ea87320a91f5959bcce96db7454db4e011
Don <don.j.olmstead@gmail.com>
2015-09-17 00:11:21 -0700

    Adding tests for annotations

--- test/all.dart
+++ test/all.dart
@@ -17,6 +17,7 @@ import 'package:test/test.dart';
 import 'src/analyzer/analyzer_metadata_test.dart' as analyzer_metadata_test;
 
 import 'src/codegen/comment_generator_test.dart' as comment_generator_test;
+import 'src/codegen/serialize_annotation_generator_test.dart' as serialize_annotation_generator_test;
 
 import 'identifier_test.dart' as identifier_test;
 
@@ -36,6 +37,7 @@ void main() {
   group('Analyzer tests', analyzer_metadata_test.main);
   group('Codegen tests', () {
     group('Comments', comment_generator_test.main);
+    group('Serialize', serialize_annotation_generator_test.main);
   });
   group('Identifier tests', identifier_test.main);
   group('Metadata tests', () {
