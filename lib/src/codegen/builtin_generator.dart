31bde82f67f373dcdba3cc4560050003e70a560e
Don <don.j.olmstead@gmail.com>
2015-09-17 21:05:25 -0700

    Adding code generation for built in values

--- lib/src/codegen/builtin_generator.dart
+++ lib/src/codegen/builtin_generator.dart
@@ -3,11 +3,71 @@
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
 
+/// Contains source code generators for built in types.
 library dogma_codegen.src.codegen.builtin_generator;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'argument_buffer.dart';
 
 //---------------------------------------------------------------------
 // Library contents
 //---------------------------------------------------------------------
 
+/// Generates source code for a built in [value].
+///
+/// The [lineBreak] value specifies whether values should go onto the new line.
+/// This is only applicable when value is a List or Map.
+String generateBuiltin(dynamic value, {bool lineBreak: false}) {
+  if (value is String) {
+    return generateString(value);
+  } else if (value is List) {
+    return generateList(value, lineBreak);
+  } else if (value is Map) {
+    return generateMap(value, lineBreak);
+  } else {
+    return value.toString();
+  }
+}
+
 /// Generates source code for a string [value].
 String generateString(String value) => '\'$value\'';
+
+/// Generates source code for list [values].
+///
+/// The [lineBreak] value specifies whether values should go onto the new line.
+String generateList(List values, bool lineBreak) {
+  var buffer = _buffer(lineBreak);
+
+  for (var value in values) {
+    buffer.write(generateBuiltin(value, lineBreak: lineBreak));
+  }
+
+  var separator = lineBreak ? '\n' : '';
+
+  return '[$separator${buffer.toString()}$separator]';
+}
+
+/// Generates source code for map [values].
+///
+/// The [lineBreak] value specifies whether values should go onto the new line.
+String generateMap(Map values, bool lineBreak) {
+  var buffer = _buffer(lineBreak);
+
+  values.forEach((key, value) {
+    var keyBuiltin = generateBuiltin(key, lineBreak: lineBreak);
+    var valueBuiltin = generateBuiltin(value, lineBreak: lineBreak);
+
+    buffer.write('$keyBuiltin:$valueBuiltin');
+  });
+
+  var separator = lineBreak ? '\n' : '';
+
+  return '{$separator${buffer.toString()}$separator}';
+}
+
+/// Creates an [ArgumentBuffer] based on whether [lineBreak]s are requested.
+ArgumentBuffer _buffer(bool lineBreak)
+    => lineBreak ? new ArgumentBuffer.lineBreak() : new ArgumentBuffer();
---
+++ test/src/codegen/builtin_generator_test.dart
@@ -0,0 +1,86 @@
+// Copyright (c) 2015, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_codegen.test.src.codegen.builtin_generator_test;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_codegen/src/codegen/builtin_generator.dart';
+import 'package:test/test.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The number of values to use.
+const int _length = 100;
+/// A list of ints.
+final List<int> _intList = [0, 1, 2, 3];
+/// Expected value of the int list.
+final String _expectedIntList = '[0,1,2,3]';
+/// A list of strings.
+final List<String> _stringList = ['foo', 'bar', 'fizz', 'buzz'];
+/// Expected value of the string list.
+final String _expectedStringList = '[\'foo\',\'bar\',\'fizz\',\'buzz\']';
+/// A map.
+final Map _map = {'ints': _intList, 'strings': _stringList};
+/// Expected value of the map.
+final String _expectedMap = '{\'ints\':$_expectedIntList,\'strings\':$_expectedStringList}';
+
+/// A test enum.
+enum TestEnum {
+  foo, bar, fizz, buzz
+}
+
+/// Test entry point.
+void main() {
+  test('Numbers', () {
+    for (var i = -_length; i < _length; ++i) {
+      expect(generateBuiltin(i), i.toString());
+
+      var d = i * 1.0;
+      expect(generateBuiltin(d), d.toString());
+
+      var f = i / _length;
+      expect(generateBuiltin(f), f.toString());
+    }
+  });
+  test('Strings', () {
+    var s = 'foo';
+    var expected = '\'$s\'';
+
+    expect(generateBuiltin(s), expected);
+    expect(generateString(s), expected);
+  });
+  test('Lists', () {
+    expect(generateBuiltin(_intList), _expectedIntList);
+    expect(generateList(_intList, false), _expectedIntList);
+
+    expect(generateBuiltin(_stringList), _expectedStringList);
+    expect(generateList(_stringList, false), _expectedStringList);
+  });
+  test('Maps', () {
+    expect(generateBuiltin(_map), _expectedMap);
+    expect(generateMap(_map, false), _expectedMap);
+  });
+  test('Enum', () {
+    expect(generateBuiltin(TestEnum.bar), TestEnum.bar.toString());
+  });
+  test('Line break', () {
+    var expected;
+    expected = '[\n${_intList.join(',\n')}\n]';
+
+    expect(generateBuiltin(_intList, lineBreak: true), expected);
+    expect(generateList(_intList, true), expected);
+
+    var map = {'a':0,'b':1};
+    expected = '{\n\'a\':0,\n\'b\':1\n}';
+
+    expect(generateBuiltin(map, lineBreak: true), expected);
+    expect(generateMap(map, true), expected);
+  });
+}
