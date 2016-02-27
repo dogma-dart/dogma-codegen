0ebf5d928f761adb94f3ca17337fe3709bcb6223
Don <don.j.olmstead@gmail.com>
2016-02-27 09:18:22 -0800

    Update copyright

--- test/src/codegen/parameter_metadata_test.dart
+++ test/src/codegen/parameter_metadata_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, the Dogma Project Authors.
+// Copyright (c) 2015-2016 the Dogma Project Authors.
 // Please see the AUTHORS file for details. All rights reserved.
 // Use of this source code is governed by a zlib license that can be found in
 // the LICENSE file.
