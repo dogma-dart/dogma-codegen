c59b41debc9a2c2bc86bdffc2f09588c9109d67e
Don <don.j.olmstead@gmail.com>
2016-02-27 08:02:51 -0800

    CLeaning up codegen library

--- lib/codegen.dart
+++ lib/codegen.dart
@@ -9,8 +9,14 @@ library dogma_codegen.codegen;
 // Exports
 //---------------------------------------------------------------------
 
-export 'src/codegen/converter_generator.dart';
-export 'src/codegen/enum_converter_generator.dart';
-export 'src/codegen/library_generator.dart';
-export 'src/codegen/mapper_generator.dart';
-export 'src/codegen/model_generator.dart';
+export 'src/codegen/annotated_metadata.dart';
+export 'src/codegen/annotation.dart';
+export 'src/codegen/argument_buffer.dart';
+export 'src/codegen/builtin_generator.dart';
+export 'src/codegen/class_metadata.dart';
+export 'src/codegen/comments.dart';
+export 'src/codegen/constructor_metadata.dart';
+export 'src/codegen/field_metadata.dart';
+export 'src/codegen/function_metadata.dart';
+export 'src/codegen/parameter_metadata.dart';
+export 'src/codegen/type_metadata.dart';
