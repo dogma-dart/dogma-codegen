137445ac9be704d1d77423f3ea7e3dd086b06dc3
Don <don.j.olmstead@gmail.com>
2016-03-30 22:33:46 -0700

    Add the io library

---
+++ lib/io.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/io/file.dart';
+export 'src/io/json.dart';
+export 'src/io/package.dart';
+export 'src/io/resource.dart';
+export 'src/io/yaml.dart';
