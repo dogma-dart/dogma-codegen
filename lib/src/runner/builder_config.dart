bb00ada8c498b4bb94be59e918c0a7b49d5ada47
Don <don.j.olmstead@gmail.com>
2016-03-31 00:08:02 -0700

    Add a base builder config

---
+++ lib/src/runner/builder_config.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Common base class for builder configurations.
+abstract class BuilderConfig {
+  /// The directory to output to.
+  String get outputDirectory;
+}
