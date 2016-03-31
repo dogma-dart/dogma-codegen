3a897f38c086ac4644456720786424af4bbd4858
Don <don.j.olmstead@gmail.com>
2016-03-31 00:18:51 -0700

    Add runner library

---
+++ lib/runner.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+/// Contains functionality to register and run builders.
+library dogma_codegen.runner;
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/runner/builder_config.dart';
+export 'src/runner/default_builder_config.dart';
+export 'src/runner/register_builder.dart';
