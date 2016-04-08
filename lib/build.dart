ca526233f24b9f5a5fdc02c5a9005e01eee42741
Don <don.j.olmstead@gmail.com>
2016-04-07 22:45:42 -0700

    Export build libraries

--- lib/build.dart
+++ lib/build.dart
@@ -10,7 +10,9 @@ library dogma_codegen.build;
 //---------------------------------------------------------------------
 
 export 'src/build/analyzer_step.dart';
+export 'src/build/asset.dart';
 export 'src/build/metadata_step.dart';
+export 'src/build/root_library_builder.dart';
 export 'src/build/skip_view_generation_step.dart';
 export 'src/build/source_builder.dart';
 export 'src/build/source_generation_step.dart';
