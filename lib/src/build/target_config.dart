e248151f2769dbeed2aebd4b4bc2d54d0e34a4e2
Don <don.j.olmstead@gmail.com>
2016-06-13 22:29:54 -0700

    Add config for a target

---
+++ lib/src/build/target_config.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Configuration for an output of the SourceBuilder.
+class TargetConfig {
+  /// Whether the target should not be outputted.
+  bool exclude;
+}
