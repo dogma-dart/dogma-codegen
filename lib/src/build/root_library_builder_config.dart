f8eafb269e1c993077ab4d74ccbd0c7e3f896b20
Don <don.j.olmstead@gmail.com>
2016-06-14 23:18:38 -0700

    Add root library config

---
+++ lib/src/build/root_library_builder_config.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'builder_config.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Configuration for a RootLibraryBuilder.
+class RootLibraryBuilderConfig extends BuilderConfig {
+  /// The relative path to the library.
+  String libraryPath;
+  /// The relative path to the directory containing the sources for the library.
+  String sourceDirectory;
+}
